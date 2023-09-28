da622ea73598af68d9183ba5ec91145137a50b1d
Kodzima <105111017+kod3ima@users.noreply.github.com>
2023-09-28 22:02:57 +1000

    Init project

---
+++ README.md
@@ -0,0 +1,18 @@
+<div align="center" width="100%">
+  <img src="https://github.com/kod3ima/kod3ima/blob/main/assets/python.gif" alt="Header">
+</div>
+
+<div class="languages" style="margin: 10px;display: flex;justify-content: center;
+align-items: center;gap: 2px;">
+<!-- Бейдж для Python -->
+<img src="https://img.shields.io/badge/Python-3.11-blue.svg" alt="Python">
+    
+<!-- Бейдж для PHP -->
+<img src="https://img.shields.io/badge/PHP-8.0-blue.svg" alt="PHP">
+    
+<!-- Бейдж для C# -->
+<img src="https://img.shields.io/badge/C%23-4.0-blue.svg" alt="C#">
+</div>
+<a href="https://kod3ima.github.io/kod3ima">
+    <img src="https://img.shields.io/badge/About me-red">
+</a>
---
+++ js/javascript.js
@@ -0,0 +1,65 @@
+const textToType = {
+    ru: {
+        title: `Биография`,
+        content: `<img src="/assets/logo.gif" /> Привет! Меня зовут Kodzima, и я - разработчик фулл-стек с горячей страстью к созданию бекенд-решений. Моя история в мире программирования началась не так давно, но с тех пор я смог достичь определенных высот и приобрести обширный опыт.
+
+        Я в настоящее время учусь в колледже, где углубляюсь в изучение компьютерных наук и смежных технологий. Моя академическая база исключительно важна для меня, так как она помогает мне постоянно совершенствовать свои навыки и быть в курсе последних тенденций в сфере разработки.
+        
+        Опыт работы - это еще одна важная часть моей биографии. У меня есть более чем 2 года стажа работы в сфере разработки. За это время я работал над разнообразными проектами, от веб-приложений до корпоративных решений, и приобрел ценный опыт в создании эффективных и надежных бекенд-систем.
+        
+        Моя страсть к программированию и разработке позволяет мне постоянно развиваться и стремиться к новым достижениям. Я уверен, что в будущем моя карьера будет продолжать развиваться, и я буду продолжать учиться и совершенствоваться в мире технологий.
+        
+        Спасибо за то, что ознакомились с моей биографией, и я надеюсь, что у меня есть возможность внести свой вклад в мир разработки программного обеспечения и создать что-то по-настоящему удивительное.`
+    },
+    en: {
+        title: `Biography`,
+        content: `<img src="/assets/logo.gif" /> Hello! My name is Kodzima, and I am a full-stack developer with a burning passion for creating backend solutions. My journey in the world of programming began not so long ago, but since then I have been able to achieve certain heights and gain extensive experience.
+
+        I am currently studying in college, where I delve into the study of computer science and related technologies. My academic foundation is extremely important to me, as it helps me constantly improve my skills and stay up-to-date with the latest trends in the development field.
+        
+        Work experience is another crucial part of my biography. I have more than 2 years of experience in the development sector. During this time, I have worked on various projects, from web applications to corporate solutions, and have gained valuable experience in building efficient and reliable backend systems.
+        
+        My passion for programming and development allows me to continually evolve and strive for new achievements. I am confident that in the future my career will continue to progress, and I will keep learning and improving in the world of technology.
+        
+        Thank you for taking the time to read my biography, and I hope that I have the opportunity to contribute to the world of software development and create something truly amazing.`
+    }
+};
+
+const outputElement = document.getElementById("output");
+
+function getBrowserLanguageContent() {
+    let lang = navigator.language.split('-')[0];  // Get only the primary language part
+
+    if (lang !== 'ru' && lang !== 'en') {
+        lang = 'en';  // Default to English for unsupported languages or Chinese
+    }
+    
+    return textToType[lang];
+}
+
+const textContent = getBrowserLanguageContent();
+
+document.title = textContent.title;
+
+function typeText(text, index) {
+    if (index < text.length) {
+        if (text.charAt(index) === '<') { 
+            let tagCloseIndex = text.indexOf('>', index);
+            let nextTagOpenIndex = text.indexOf('<', index + 1);
+            
+            while (nextTagOpenIndex !== -1 && nextTagOpenIndex < tagCloseIndex) {
+                tagCloseIndex = text.indexOf('>', tagCloseIndex + 1);
+                nextTagOpenIndex = text.indexOf('<', nextTagOpenIndex + 1);
+            }
+
+            outputElement.insertAdjacentHTML('beforeend', text.substring(index, tagCloseIndex + 1));
+            setTimeout(() => typeText(text, tagCloseIndex + 1), 20);
+            return;
+        }
+
+        outputElement.insertAdjacentHTML('beforeend', text.charAt(index)); 
+        setTimeout(() => typeText(text, index + 1), 20);
+    }
+}
+
+typeText(textContent.content, 0);
