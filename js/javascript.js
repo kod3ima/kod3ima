4648603ea716ddf7814e2ef5acce516dc12c9b5f
Kodzima <105111017+kod3ima@users.noreply.github.com>
2023-09-28 22:07:31 +1000

    Update javascript.js - Insert url to logo

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,7 @@
 const textToType = {
     ru: {
         title: `Биография`,
-        content: `<img src="/assets/logo.gif" /> Привет! Меня зовут Kodzima, и я - разработчик фулл-стек с горячей страстью к созданию бекенд-решений. Моя история в мире программирования началась не так давно, но с тех пор я смог достичь определенных высот и приобрести обширный опыт.
+        content: `<img src="https://github.com/kod3ima/kod3ima/blob/main/assets/logo.gif?raw=true" /> Привет! Меня зовут Kodzima, и я - разработчик фулл-стек с горячей страстью к созданию бекенд-решений. Моя история в мире программирования началась не так давно, но с тех пор я смог достичь определенных высот и приобрести обширный опыт.
 
         Я в настоящее время учусь в колледже, где углубляюсь в изучение компьютерных наук и смежных технологий. Моя академическая база исключительно важна для меня, так как она помогает мне постоянно совершенствовать свои навыки и быть в курсе последних тенденций в сфере разработки.
         
@@ -13,7 +13,7 @@ const textToType = {
     },
     en: {
         title: `Biography`,
-        content: `<img src="/assets/logo.gif" /> Hello! My name is Kodzima, and I am a full-stack developer with a burning passion for creating backend solutions. My journey in the world of programming began not so long ago, but since then I have been able to achieve certain heights and gain extensive experience.
+        content: `<img src="https://github.com/kod3ima/kod3ima/blob/main/assets/logo.gif?raw=true" /> Hello! My name is Kodzima, and I am a full-stack developer with a burning passion for creating backend solutions. My journey in the world of programming began not so long ago, but since then I have been able to achieve certain heights and gain extensive experience.
 
         I am currently studying in college, where I delve into the study of computer science and related technologies. My academic foundation is extremely important to me, as it helps me constantly improve my skills and stay up-to-date with the latest trends in the development field.
         
